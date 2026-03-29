1a46d9ed2b2b7161498e321d33473566cdf14eb4
Marcus Alsterfjord <marcus@marcussite.se>
2026-03-29 21:46:33 +0200

    Move WIFI_SSID from config.h to secrets.h for better security. Update secrets.h.example accordingly.

--- config.h
+++ config.h
@@ -1,7 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define WIFI_SSID "FT12"
 #define API_URL       "https://n8nflow.duckdns.org/webhook/21478d4c-27c4-4c74-9ac9-def4ee246fb5"
 #define API_URL_META  API_URL "?type=meta"
 #define API_URL_IMAGE API_URL "?type=image"
