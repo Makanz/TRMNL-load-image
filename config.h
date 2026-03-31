64f0fb77036a491531ab9b0275d8dfd9480ac93e
Marcus Alsterfjord <marcus@marcussite.se>
2026-03-31 22:40:06 +0200

    Fix double-free in fetchRawImageAndDisplay
    - Remove premature sink.clear() that freed buffer before PNG decode
    - Manually free decodedBuffer and imgBuffer at end of function
    - Set sink.buf=nullptr to prevent double-free in BufStream destructor
    
    Also adds partial image update feature with imageDiff API.

--- config.h
+++ config.h
@@ -4,6 +4,10 @@
 #define API_URL       "https://n8nflow.duckdns.org/webhook/21478d4c-27c4-4c74-9ac9-def4ee246fb5"
 #define API_URL_META  API_URL "?type=meta"
 #define API_URL_IMAGE API_URL "?type=image"
+#define API_URL_DIFF  API_URL "?type=imageDiff"
+#define API_URL_REGION API_URL "?type=imageRegion"
+
+#define FULL_FETCH_INTERVAL_HOURS 6
 
 #define REFRESH_INTERVAL_MINUTES 1
 
